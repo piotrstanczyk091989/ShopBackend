dd36a5de602f38f7a022a9fb90d1a20007e40b03
piotrstanczyk091989 <piotr.stanczyk.09@gmail.com>
2023-11-29 20:47:12 +0100

    Products pagination

--- src/main/java/pl/javaps/shop/product/service/ProductService.java
+++ src/main/java/pl/javaps/shop/product/service/ProductService.java
@@ -1,11 +1,11 @@
 package pl.javaps.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.javaps.shop.product.model.Product;
 import pl.javaps.shop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +13,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts(){
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable){
+        return productRepository.findAll(pageable);
     }
 }
