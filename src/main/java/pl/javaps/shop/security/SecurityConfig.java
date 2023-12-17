45c39a5384ad18e8866f6bff78035a82ae124b3d
piotrstanczyk091989 <piotr.stanczyk.09@gmail.com>
2023-12-17 21:29:41 +0100

    Administrative user login (JWT token)

---
+++ src/main/java/pl/javaps/shop/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package pl.javaps.shop.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+
+    private String secret;
+
+    public SecurityConfig(@Value("${jwt.secret}") String secret) {
+        this.secret = secret;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           AuthenticationManager authenticationManager,
+                                           UserDetailsService userDetailsService) throws Exception {
+        http.authorizeRequests(authorize -> authorize
+                .antMatchers("/admin/**").authenticated()
+                .anyRequest().permitAll()
+        );
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilter(new JwtAuthorizationFilter(authenticationManager, userDetailsService, secret));
+        //System.out.println(new BCryptPasswordEncoder().encode("test"));
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("test")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin);
+        return new JdbcUserDetailsManager(dataSource);
+    }
+}
